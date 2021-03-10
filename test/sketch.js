57bcd8bc6bc69b72f11c7b2235699d9c46e6c49d
Paolo Camerin <paolo92camerin@gmail.com>
2021-03-10 20:29:42 +0100

    Updated names and sketches

--- test/sketch.js
+++ test/sketch.js
@@ -1,6 +1,7 @@
 let serial;
 let latestData = "waiting for data";
 let dataflow =[];
+let buffer =[];
 function setup() {
  createCanvas(windowWidth, windowHeight);
 
@@ -56,25 +57,40 @@ function gotData() {
 }
 
 function draw() {
- background(255,255,255);
+ background(245);
 noStroke();
 fill(21);
  text(latestData, 10, 20);
  text(dataflow.length, 10, 40);
- dataflow.push(latestData);
+ text(buffer.length, 10, 60);
+ buffer.push(latestData);
 
- 
- noFill();
-stroke(21);
-strokeWeight(4);
-beginShape();
- for(let v = 0; v < dataflow.length; v++){
-vertex(width/dataflow.length*v,height/3*2-(dataflow[v]));
-
+ if(buffer.length>30){
+     let a = 0; 
+     for(let b of buffer){
+        a+=int(b);
+     }
+     
+     dataflow.push(a/buffer.length);
+     buffer.shift();
  }
- endShape(OPEN);
 
- if(dataflow.length>100){
+
+ if(dataflow){
+    noFill();
+    stroke(21);
+    strokeWeight(4);
+    ellipse(width/2,height/2,dataflow[dataflow.length-1],dataflow[dataflow.length-1]);
+    beginShape();
+     for(let v = 0; v < dataflow.length; v++){
+    vertex(width/dataflow.length*v,height-50-(dataflow[v]));
+    
+     }
+     endShape(OPEN);
+ }
+
+
+ if(dataflow.length>60){
     dataflow.shift();
  }
 }
