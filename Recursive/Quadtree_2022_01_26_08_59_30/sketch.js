2c1e94d54a193a49e89786752a36f7c4a4bfe545
Paolo Camerin <paolo92camerin@gmail.com>
2022-01-26 15:41:11 +0100

    update recursive sketch

--- Recursive/Quadtree_2022_01_26_08_59_30/sketch.js
+++ Recursive/Quadtree_2022_01_26_08_59_30/sketch.js
@@ -1,20 +1,28 @@
-function setup() {
-  createCanvas(1000, 1000);
+let can; 
 
+let ran =[];
+function setup() {
+  can = createCanvas(windowWidth, 1000);
+  can.parent("canvasContainer");
   //rectMode(CENTER);
+
+  for(let i = 0; i < 10; i++){
+  ran.push(floor(random(100,300)));
+  }
+  print(ran);
 }
 
 function draw() {
   background(220);
   //randomSeed(123123);
 
-  drawRectangle(0,0,width,height,8,height/2+(height/4)*sin(frameCount/100));
+  drawRectangle(0,0,width,height,8,height/2+(height/4)*sin(frameCount/ran[6]));
 
 }
 
 function drawRectangle(x,y,w,h,depth,mid){
 
-  print(mid);
+
   fill(245);
   stroke(0);
   strokeWeight(2);
@@ -32,18 +40,18 @@ function drawRectangle(x,y,w,h,depth,mid){
     // 'level' decreases by 1 at every step and thus makes the terminating condition
     // attainable
    
-      console.log("Depth = " + depth);
+      //console.log("Depth = " + depth);
       depth = depth - 1;  
       if(depth%2 === 0){
         //horizontal split
-        console.log("splitting horizontal");
-        drawRectangle(x  , y , mid, h, depth,h/2+(h/4)*sin(frameCount/70));
-        drawRectangle(x+mid  , y , w-mid, h,depth,h/2+(h/4)*sin(frameCount/100));
+        //console.log("splitting horizontal");
+        drawRectangle(x  , y , mid, h, depth,h/2+(h/4)*sin(frameCount/ran[1]));
+        drawRectangle(x+mid  , y , w-mid, h,depth,h/2+(h/4)*sin(frameCount/ran[3]));
       }else{
         //vertical split
-        console.log("splitting veritcal");
-        drawRectangle(x  , y , w, mid, depth,w/2+(w/4)*sin(frameCount/200));
-        drawRectangle(x  , y+mid , w, h-mid,depth,w/2+(w/4)*sin(frameCount/250));
+        //console.log("splitting veritcal");
+        drawRectangle(x  , y , w, mid, depth,w/2+(w/4)*sin(frameCount/ran[2]));
+        drawRectangle(x  , y+mid , w, h-mid,depth,w/2+(w/4)*sin(frameCount/ran[5]));
 
       }
     
@@ -51,6 +59,6 @@ function drawRectangle(x,y,w,h,depth,mid){
     
 
   }
-  console.log("End of function");
+  //console.log("End of function");
   
 }
