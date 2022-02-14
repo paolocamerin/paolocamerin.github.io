97f3e160eac3140ca094055f2034ff819beaeca8
Paolo Camerin <paolo92camerin@gmail.com>
2022-02-14 16:33:05 +0100

    Added example

--- RecursiveExamples/Recursive_3/sketch.js
+++ RecursiveExamples/Recursive_3/sketch.js
@@ -1,6 +1,6 @@
-let can; 
+let can;
 let d = 9;
-let ran =[];
+let ran = [];
 function setup() {
 
 
@@ -8,8 +8,8 @@ function setup() {
   can.parent("canvasContainer");
   //rectMode(CENTER);
 
-  for(let i = 0; i < 10; i++){
-  ran.push(floor(random(50,150)));
+  for (let i = 0; i < 10; i++) {
+    ran.push(floor(random(50, 150)));
   }
   print(ran);
 }
@@ -17,66 +17,66 @@ function setup() {
 function draw() {
   background(245);
   //randomSeed(123123);
-  if(d%2==0){
-    drawRectangle(0,0,width,height,d,height/2+(height/4)*sin(frameCount/ran[6])*noise(frameCount/ran[7]));
+  if (d % 2 == 0) {
+    drawRectangle(0, 0, width, height, d, height / 2 + (height / 4) * sin(frameCount / ran[6]) * noise(frameCount / ran[7]));
     //text("EVEN",20,20);
-  }else{
-    
-    drawRectangle(0,0,width,height,d,width/2+(height/4)*sin(frameCount/ran[6])*noise(frameCount/ran[7]));
+  } else {
+
+    drawRectangle(0, 0, width, height, d, width / 2 + (height / 4) * sin(frameCount / ran[6]) * noise(frameCount / ran[7]));
     //text("ODD",20,20);
   }
- 
+
 
 
 }
 
-function drawRectangle(x,y,w,h,depth,mid){
+function drawRectangle(x, y, w, h, depth, mid) {
 
 
 
   stroke(21);
   strokeWeight(1);
-  fill(map(h,0,height,0,600),map(w,0,width,0,600),100);
-  rect(x,y,w,h);
+  fill(map(h, 0, height, 0, 600), map(w, 0, width, 0, 600), 100);
+  rect(x, y, w, h);
 
   noStroke();
   fill(21);
-  ellipse(x+w/2,y+h/2,5,5);
+  ellipse(x + w / 2, y + h / 2, 5, 5);
 
-  
-  
-  
-  if (depth > 1) {  
+
+
+
+  if (depth > 1) {
     // 'level' decreases by 1 at every step and thus makes the terminating condition
     // attainable
-   
-      //console.log("Depth = " + depth);
-      depth = depth - 1;  
-      if(depth%2 === 0){
-        //horizontal split
 
-        //console.log("splitting horizontal");
-        drawRectangle(x  , y , mid, h, depth,h/2+(h/4)*sin(frameCount/ran[1])*noise(frameCount/ran[2]));
-        drawRectangle(x+mid  , y , w-mid, h,depth,h/2+(h/4)*sin(frameCount/ran[3])*noise(frameCount/ran[5]));
-      }else{
-        //vertical split
+    //console.log("Depth = " + depth);
+    depth = depth - 1;
+    if (depth % 2 === 0) {
+      //horizontal split
 
-        //console.log("splitting veritcal");
-        drawRectangle(x  , y , w, mid, depth,w/2+(w/4)*sin(frameCount/ran[2])*noise(frameCount/ran[4]));
-        drawRectangle(x  , y+mid , w, h-mid,depth,w/2+(w/4)*sin(frameCount/ran[5])*noise(frameCount/ran[9]));
+      //console.log("splitting horizontal");
+      drawRectangle(x, y, mid, h, depth, h / 2 + (h / 4) * sin(frameCount / ran[1]) * noise(frameCount / ran[2]));
+      drawRectangle(x + mid, y, w - mid, h, depth, h / 2 + (h / 4) * sin(frameCount / ran[3]) * noise(frameCount / ran[5]));
+    } else {
+      //vertical split
 
-      }
-    
-     
-    
-    
+      //console.log("splitting veritcal");
+      drawRectangle(x, y, w, mid, depth, w / 2 + (w / 4) * sin(frameCount / ran[2]) * noise(frameCount / ran[4]));
+      drawRectangle(x, y + mid, w, h - mid, depth, w / 2 + (w / 4) * sin(frameCount / ran[5]) * noise(frameCount / ran[9]));
+
+    }
+
+
+
+
 
   }
-  
+
   //console.log("End of function");
-  
+
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
---
+++ RecursiveExamples/Recursive_5/sketch.js
@@ -0,0 +1,87 @@
+let can;
+let d = 9;
+let ran = [];
+function setup() {
+
+
+  can = createCanvas(windowWidth, windowHeight);
+  can.parent("canvasContainer");
+  //rectMode(CENTER);
+
+  for (let i = 0; i < 10; i++) {
+    ran.push(floor(random(50, 150)));
+  }
+  print(ran);
+}
+
+function draw() {
+  background(21);
+  //randomSeed(123123);
+  if (d % 2 == 0) {
+    drawRectangle(0, 0, width, height, d, height / 2 + (height / 4) * sin(frameCount / ran[6]) * noise(frameCount / ran[7]));
+    //text("EVEN",20,20);
+  } else {
+
+    drawRectangle(0, 0, width, height, d, width / 2 + (height / 4) * sin(frameCount / ran[6]) * noise(frameCount / ran[7]));
+    //text("ODD",20,20);
+  }
+
+
+
+}
+
+function drawRectangle(x, y, w, h, depth, mid) {
+
+
+
+  stroke(21);
+  strokeWeight(1);
+  fill(map(h, 0, height, 0, 600), map(w, 0, width, 0, 600), 100);
+  if (depth == 1) {
+
+    rect(x + 5, y + 5, w - 5, h - 5, 20);
+
+  }
+
+
+  noStroke();
+  fill(21);
+  //ellipse(x + w / 2, y + h / 2, 5, 5);
+
+
+
+
+  if (depth > 1) {
+    // 'level' decreases by 1 at every step and thus makes the terminating condition
+    // attainable
+
+    //console.log("Depth = " + depth);
+    depth = depth - 1;
+    if (depth % 2 === 0) {
+      //horizontal split
+
+      //console.log("splitting horizontal");
+      drawRectangle(x, y, mid, h, depth, h / 2 + (h / 4) * sin(frameCount / ran[1]) * noise(frameCount / ran[2]));
+      drawRectangle(x + mid, y, w - mid, h, depth, h / 2 + (h / 4) * sin(frameCount / ran[3]) * noise(frameCount / ran[5]));
+    } else {
+      //vertical split
+
+      //console.log("splitting veritcal");
+      drawRectangle(x, y, w, mid, depth, w / 2 + (w / 4) * sin(frameCount / ran[2]) * noise(frameCount / ran[4]));
+      drawRectangle(x, y + mid, w, h - mid, depth, w / 2 + (w / 4) * sin(frameCount / ran[5]) * noise(frameCount / ran[9]));
+
+    }
+
+
+
+
+
+  }
+
+  //console.log("End of function");
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
